a114f9f7b5f2586b98bab77a6f6aaec8b131de8a
BielCR <gabrielcr18@outlook.com>
2021-03-08 21:33:23 -0300

    Arrumando o icone da página

--- pages/agendamentos/CRUD_Agendamento.php
+++ pages/agendamentos/CRUD_Agendamento.php
@@ -8,7 +8,7 @@ include "../conexao.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="./styles/media/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../styles/media/favicon.ico" type="image/x-icon">
     <title>Agendamento</title>
     <link rel="stylesheet" href="../styles/main.css">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
